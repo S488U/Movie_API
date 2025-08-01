ff14bcd225772ac9c3c2074c81c9ec5b3461abdb
S488U <shahabassabbu12@gmail.com>
2025-08-01 15:53:03 +0530

    Refactor: Improve event listener setup and variable scoping in login.js

--- assets/script/login.js
+++ assets/script/login.js
@@ -1,25 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-var submit = document.getElementById("submit");
+    let submit;
+    if (document.getElementById("submit")) {
+        submit = document.getElementById("submit");
+        submit.addEventListener("click", (event) => {
+            event.preventDefault();
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
 
-submit.addEventListener("click", (event) => {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    if(!name || !email || !password ) {
-        alert("Please enter the email and password");
-    } else {
-        var user = {
-            name: name,
-            email : email,
-            password: password
-        }
-        localStorage.setItem("userInfo", JSON.stringify(user));
-        window.location.href = "./user.html";
+            if (!name || !email || !password) {
+                alert("Please enter the email and password");
+            } else {
+                let user = {
+                    name: name,
+                    email: email,
+                    password: password
+                }
+                localStorage.setItem("userInfo", JSON.stringify(user));
+                window.location.href = "./user.html";
+            }
+        });
     }
 });
-
-
-
-
